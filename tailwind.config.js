0f87225ad4217da0630d89b82cb8a85edf44c2d7
NicolasFrouin <frouinnicolas@gmaiL.com>
2023-12-13 14:42:58 +0100

    added tailwind-scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: "class",
-	content: ["./assets/**/*.js", "./templates/**/*.html.twig", "./node_modules/flowbite/**/*.js"],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("flowbite/plugin")],
+    darkMode: "class",
+    content: ["./assets/**/*.js", "./templates/**/*.html.twig", "./node_modules/flowbite/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")({nocompatible: true})],
 };
