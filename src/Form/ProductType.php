ec6e9a649ab34aebe67e36bfe31453f2256d822b
YvesMu <EternalOfIce@gmail.com>
2024-02-27 09:57:29 +0100

    COrrection problème CSS TEST

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -15,18 +15,18 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('ref', TextType::class, ['label' => 'Référence', 'attr' => ['placeholder' => 'Référence du produit']])
-            ->add('name', TextType::class, ['label' => 'Nom', 'attr' => ['placeholder' => 'Nom du produit']])
-            ->add('description', TextType::class, ['label' => 'Description', 'attr' => ['placeholder' => 'Description du produit']])
-            ->add('taxRate', NumberType::class, ['label' => 'Taux de TVA', 'attr' => ['placeholder' => 'Taux de TVA du produit']])
-            ->add('priceNoTax', NumberType::class, ['label' => 'Prix HT', 'attr' => ['placeholder' => 'Prix HT du produit']])
-            ->add('priceWithTax', NumberType::class, ['label' => 'Prix TTC', 'attr' => ['placeholder' => 'Prix TTC du produit']])
-            ->add('weight', NumberType::class, ['label' => 'Poids', 'attr' => ['placeholder' => 'Poids du produit']])
-            ->add('height', NumberType::class, ['label' => 'Hauteur', 'attr' => ['placeholder' => 'Hauteur du produit']])
-            ->add('width', NumberType::class, ['label' => 'Largeur', 'attr' => ['placeholder' => 'Largeur du produit']])
-            ->add('depth', NumberType::class, ['label' => 'Profondeur', 'attr' => ['placeholder' => 'Profondeur du produit']])
-            ->add('stock', NumberType::class, ['label' => 'Stock', 'attr' => ['placeholder' => 'Stock du produit']])
-            ->add('active', null, ['label' => 'Actif', 'attr' => ['placeholder' => 'Actif']]);
+            ->add('ref', TextType::class, ['label' => 'Référence', 'attr' => ['placeholder' => 'Référence du produit'], 'label_attr' => ['class' => 'text-red'],])
+            ->add('name', TextType::class, ['label' => 'Nom', 'attr' => ['placeholder' => 'Nom du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('description', TextType::class, ['label' => 'Description', 'attr' => ['placeholder' => 'Description du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('taxRate', NumberType::class, ['label' => 'Taux de TVA', 'attr' => ['placeholder' => 'Taux de TVA du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('priceNoTax', NumberType::class, ['label' => 'Prix HT', 'attr' => ['placeholder' => 'Prix HT du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('priceWithTax', NumberType::class, ['label' => 'Prix TTC', 'attr' => ['placeholder' => 'Prix TTC du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('weight', NumberType::class, ['label' => 'Poids', 'attr' => ['placeholder' => 'Poids du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('height', NumberType::class, ['label' => 'Hauteur', 'attr' => ['placeholder' => 'Hauteur du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('width', NumberType::class, ['label' => 'Largeur', 'attr' => ['placeholder' => 'Largeur du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('depth', NumberType::class, ['label' => 'Profondeur', 'attr' => ['placeholder' => 'Profondeur du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('stock', NumberType::class, ['label' => 'Stock', 'attr' => ['placeholder' => 'Stock du produit'], 'label_attr' => ['class' => 'text-white']])
+            ->add('active', null, ['label' => 'Actif', 'attr' => ['placeholder' => 'Actif'], 'label_attr' => ['class' => 'text-white']]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
