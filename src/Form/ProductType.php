8dfd0c2fc55ae0e3a02fd9855d1cd669fd4186f2
YvesMu <EternalOfIce@gmail.com>
2024-02-29 10:26:46 +0100

    Correction du footer & commencement du changement des pages edit de produit
    Correction du footer

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -15,18 +15,18 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('ref', TextType::class, ['label' => 'Référence', 'attr' => ['placeholder' => 'Référence du produit'], 'label_attr' => ['class' => 'text-red'],])
-            ->add('name', TextType::class, ['label' => 'Nom', 'attr' => ['placeholder' => 'Nom du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('description', TextType::class, ['label' => 'Description', 'attr' => ['placeholder' => 'Description du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('taxRate', NumberType::class, ['label' => 'Taux de TVA', 'attr' => ['placeholder' => 'Taux de TVA du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('priceNoTax', NumberType::class, ['label' => 'Prix HT', 'attr' => ['placeholder' => 'Prix HT du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('priceWithTax', NumberType::class, ['label' => 'Prix TTC', 'attr' => ['placeholder' => 'Prix TTC du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('weight', NumberType::class, ['label' => 'Poids', 'attr' => ['placeholder' => 'Poids du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('height', NumberType::class, ['label' => 'Hauteur', 'attr' => ['placeholder' => 'Hauteur du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('width', NumberType::class, ['label' => 'Largeur', 'attr' => ['placeholder' => 'Largeur du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('depth', NumberType::class, ['label' => 'Profondeur', 'attr' => ['placeholder' => 'Profondeur du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('stock', NumberType::class, ['label' => 'Stock', 'attr' => ['placeholder' => 'Stock du produit'], 'label_attr' => ['class' => 'text-white']])
-            ->add('active', null, ['label' => 'Actif', 'attr' => ['placeholder' => 'Actif'], 'label_attr' => ['class' => 'text-white']]);
+            ->add('ref', TextType::class, ['label' => 'Référence', 'attr' => ['placeholder' => 'Référence du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white'],])
+            ->add('name', TextType::class, ['label' => 'Nom', 'attr' => ['placeholder' => 'Nom du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('description', TextType::class, ['label' => 'Description', 'attr' => ['placeholder' => 'Description du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('taxRate', NumberType::class, ['label' => 'Taux de TVA', 'attr' => ['placeholder' => 'Taux de TVA du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('priceNoTax', NumberType::class, ['label' => 'Prix HT', 'attr' => ['placeholder' => 'Prix HT du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('priceWithTax', NumberType::class, ['label' => 'Prix TTC', 'attr' => ['placeholder' => 'Prix TTC du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('weight', NumberType::class, ['label' => 'Poids', 'attr' => ['placeholder' => 'Poids du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('height', NumberType::class, ['label' => 'Hauteur', 'attr' => ['placeholder' => 'Hauteur du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('width', NumberType::class, ['label' => 'Largeur', 'attr' => ['placeholder' => 'Largeur du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('depth', NumberType::class, ['label' => 'Profondeur', 'attr' => ['placeholder' => 'Profondeur du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('stock', NumberType::class, ['label' => 'Stock', 'attr' => ['placeholder' => 'Stock du produit'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']])
+            ->add('active', null, ['label' => 'Actif', 'attr' => ['placeholder' => 'Actif'], 'label_attr' => ['class' => 'inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 dark:text-white']]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
