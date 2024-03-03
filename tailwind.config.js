ce953a1ea447d98d24c9f232155a540e0b1dab2b
quentinParavano <quentin.paravano@gmail.com>
2024-03-03 21:11:35 +0100

    theme01

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
     darkMode: "class",
     content: ["./assets/**/*.js", "./templates/**/*.html.twig", "./node_modules/flowbite/**/*.js"],
-    theme: {
-        extend: {},
+    theme01: {
+        colors: {
+            'blue': '#1fb6ff',
+            'purple': '#7e5bef',
+            'pink': '#ff49db',
+            'orange': '#ff7849',
+            'green': '#13ce66',
+            'yellow': '#ffc82c',
+            'gray-dark': '#273444',
+            'gray': '#8492a6',
+            'gray-light': '#d3dce6',
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        roundedNone: { 'border-radius': '0' },
+        rounded: { 'border-radius': '25rem' },
+        roundedFull: { 'border-radius': '9999px' }
     },
     plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")({nocompatible: true})],
 };
